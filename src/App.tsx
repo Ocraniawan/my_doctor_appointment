7dd6125dc32fdefd4eb1d1d04d2af30e68b59b7e
Ocraniawan Patattan <Ocraniawanpatattan@gmail.com>
2022-11-06 22:44:10 +0700

    keep footer on bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import DetailDoctor from "./pages/DetailDoctorPage";
 
 export default function App() {
   return (
     <Router>
       <div className="bg-secondary">
         <Navbar />
-        <div className="mt-14">
+        <div className="mt-14 min-h-screen">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/detail-doctor/:id" element={<DetailDoctor />} />
           </Routes>
         </div>
         <Footer />
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-200 text-gray-900">
+    <div className="bg-gray-200 text-gray-900 left-0 right-0 bottom-0">
       <div className=" pb-20 pt-10 text-center text-sm">
         Made with love © {new Date().getFullYear()},{" "}
         <span className="text-primary">Ocraniawan Patattan</span>
