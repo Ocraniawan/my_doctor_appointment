dc9404d266346f03ff630fcf3fa3dcce73f90aae
Ocraniawan Patattan <Ocraniawanpatattan@gmail.com>
2022-11-07 21:26:06 +0700

    tidy up ui doctor details page

--- src/components/BookAppointment/FormBooking/index.tsx
+++ src/components/BookAppointment/FormBooking/index.tsx
@@ -80,103 +80,102 @@ export default function FormBooking(props: BookingProps) {
   };
 
   return (
-    <div>
-      <div className="text-start md:pl-4 mb-4">
-        <div className="font-bold text-xl pb-1 text-gray-700">
-          Booking Appointment
-        </div>
-        <hr className="pb-4" />
-        <form onSubmit={handleSubmit}>
-          <div className="block my-2">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
-            >
-              Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
-                  focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-            />
-            <text className="text-pink-600 text-xs mb-2 invisible">
-              username is required
-            </text>
-          </div>
-
-          <div className="block my-2">
-            <label
-              htmlFor="date-book"
-              className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
-            >
-              Date Booking
-            </label>
-            <input
-              className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
-                  focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              name="date-book"
-              type="date"
-              required
-              min={disablePastDate()}
-              onChange={handleChangeDate}
-              value={dateBooking}
-            />
-            <text
-              className={`text-pink-600 text-xs mb-2 ${
-                invalidDate ? "visible" : "invisible"
-              }`}
-            >
-              Doctor isn't opening on that date, place choose another date!
-            </text>
-          </div>
-          <div className="block mt-2">
-            <label
-              htmlFor="time-book"
-              className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
-            >
-              Time Booking
-            </label>
-            <select
-              name="time-book"
-              id="time"
-              required
-              disabled={!dateBooking || invalidDate}
-              value={timeBooking}
-              onChange={handleTimeChange}
-              className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
-                                focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-            >
-              <option value={0}>Select</option>
-              {hours &&
-                hours.map((hour: number, index: Key) => (
-                  <option key={index} value={hour}>
-                    {hour}:00
-                  </option>
-                ))}
-            </select>
-            <text
-              className={`text-pink-600 text-xs mb-2 ${
-                invalidTime ? "visible" : "invisible"
-              }`}
-            >
-              Doctor isn't opening on that time, place choose another time!
-            </text>
-          </div>
-          <div className="pt-4">
-            <button
-              disabled={!dateBooking || invalidDate || invalidTime}
-              type="submit"
-              className="float-right bg-primary text-sm h-10 rounded-xl text-white w-fit px-10 text-center items-center flex font-semibold disabled:bg-gray-300"
-            >
-              Book
-            </button>
-          </div>
-        </form>
+    <div className="border p-4 rounded-lg shadow-sm bg-secondary h-[480px] md:h-auto">
+      <div className="font-bold text-xl pb-1 text-gray-700">
+        Booking Appointment
       </div>
+      <hr className="pb-4" />
+      <form onSubmit={handleSubmit}>
+        <div className="block my-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
+          >
+            Name
+          </label>
+          <input
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
+                  focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+          />
+          <text className="text-pink-600 text-xs mb-2 invisible">
+            username is required
+          </text>
+        </div>
+
+        <div className="block my-2">
+          <label
+            htmlFor="date-book"
+            className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
+          >
+            Date Booking
+          </label>
+          <input
+            className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
+                  focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            name="date-book"
+            type="date"
+            required
+            min={disablePastDate()}
+            onChange={handleChangeDate}
+            value={dateBooking}
+          />
+          <text
+            className={`text-pink-600 text-xs mb-2 ${
+              invalidDate ? "visible" : "invisible"
+            }`}
+          >
+            Doctor isn't opening on that date, place choose another date!
+          </text>
+        </div>
+
+        <div className="block my-2">
+          <label
+            htmlFor="time-book"
+            className="block text-sm font-medium text-slate-700 after:content-['*'] after:ml-0.5 after:text-red-500"
+          >
+            Time Booking
+          </label>
+          <select
+            name="time-book"
+            id="time"
+            required
+            disabled={!dateBooking || invalidDate}
+            value={timeBooking}
+            onChange={handleTimeChange}
+            className="mt-1 px-3 py-2 w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500
+                                focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+          >
+            <option value={0}>Select</option>
+            {hours &&
+              hours.map((hour: number, index: Key) => (
+                <option key={index} value={hour}>
+                  {hour}:00
+                </option>
+              ))}
+          </select>
+          <text
+            className={`text-pink-600 text-xs mb-2 ${
+              invalidTime ? "visible" : "invisible"
+            }`}
+          >
+            Doctor isn't opening on that time, place choose another time!
+          </text>
+        </div>
+        <div className="block">
+          <button
+            disabled={!dateBooking || invalidDate || invalidTime}
+            type="submit"
+            className="float-right bg-primary text-sm h-10 rounded-xl text-white w-fit px-10 text-center items-center flex font-semibold disabled:bg-gray-300"
+          >
+            Book
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/pages/DetailDoctorPage/index.tsx
+++ src/pages/DetailDoctorPage/index.tsx
@@ -80,63 +80,65 @@ export default function DetailDoctor() {
         handleModal={handleModalAppointment}
       />
       {/* start here */}
-      <div className="md:flex text-start justify-between px-4 mb-4 md:px-10 md:mb-10">
-        <div className="mx-2 mb-4">
-          <img
-            src="/assets/images/doctor-profile.png"
-            alt="doctor"
-            className="rounded-2xl shadow-sm h-96 w-full"
-          />
-        </div>
-        {data && (
-          <div className="px-2 md:pr-4 mb-4">
-            <div className="font-bold text-2xl pb-1 text-primary">
-              {capitalizeStr(data.name)}
-            </div>
-            <hr className="pb-4" />
-            <div className="flex justify-start text-base items-center pb-2 font-medium text-gray-700">
-              <SlLocationPin size={16} color={"#ff0068"} />
-              <span className="pl-4">
-                {data.address.line_1}
-                {" - "}
-                {data.address.line_2}
-                {" - "}
-                {data.address.district}
-              </span>
-            </div>
-            {/* availibility */}
-            <div className="text-base font-medium mb-4 text-gray-700">
-              <div className="flex justify-start items-center pb-2">
-                <MdOutlineWatchLater size={16} color={"#ff0068"} />
-                <span className="pl-4">Opening Hours</span>
-              </div>
-              <div className="pl-8">
-                {data.opening_hours.map((opening: any, index: Key) => (
-                  <div key={index} className="flex w-full pb-1">
-                    <span className="mr-2 w-20 font-medium">
-                      {capitalizeStr(opening.day)}
-                    </span>
-                    {opening.isClosed ? (
-                      <span className="text-primary font-medium">Closed</span>
-                    ) : (
-                      <span className="font-medium">
-                        {opening.start} - {opening.end}
-                      </span>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-            <button
-              onClick={() => setMakeAppointment(true)}
-              className="bg-white text-sm h-10 border border-primary rounded-xl text-primary w-full px-4 text-center items-center font-semibold
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-              disabled={makeAppointment}
-            >
-              Make an Appointment
-            </button>
+      <div className="md:flex text-start justify-between px-2 mb-4 md:px-10 md:mb-10">
+        <div className="md:flex border rounded-lg shadow-sm py-2 md:mr-2 mb-4 md:mb-auto">
+          <div className="mx-2 mb-4">
+            <img
+              src="/assets/images/doctor-profile.png"
+              alt="doctor"
+              className="rounded-2xl shadow-sm h-96 w-full"
+            />
           </div>
-        )}
+          {data && (
+            <div className="px-2 md:pr-4 mb-4">
+              <div className="font-bold text-2xl pb-1 text-primary">
+                {capitalizeStr(data.name)}
+              </div>
+              <hr className="pb-4" />
+              <div className="flex justify-start text-base items-center pb-2 font-medium text-gray-700">
+                <SlLocationPin size={16} color={"#ff0068"} />
+                <span className="pl-4">
+                  {data.address.line_1}
+                  {" - "}
+                  {data.address.line_2}
+                  {" - "}
+                  {data.address.district}
+                </span>
+              </div>
+              {/* availibility */}
+              <div className="text-base font-medium mb-4 text-gray-700">
+                <div className="flex justify-start items-center pb-2">
+                  <MdOutlineWatchLater size={16} color={"#ff0068"} />
+                  <span className="pl-4">Opening Hours</span>
+                </div>
+                <div className="pl-8">
+                  {data.opening_hours.map((opening: any, index: Key) => (
+                    <div key={index} className="flex w-full pb-1">
+                      <span className="mr-2 w-20 font-medium">
+                        {capitalizeStr(opening.day)}
+                      </span>
+                      {opening.isClosed ? (
+                        <span className="text-primary font-medium">Closed</span>
+                      ) : (
+                        <span className="font-medium">
+                          {opening.start} - {opening.end}
+                        </span>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <button
+                onClick={() => setMakeAppointment(true)}
+                className="bg-white text-sm h-10 border border-primary rounded-xl text-primary w-full px-4 text-center items-center font-semibold
+                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                disabled={makeAppointment}
+              >
+                Make an Appointment
+              </button>
+            </div>
+          )}
+        </div>
         {!makeAppointment ? (
           <div className="mb-4">
             <img
@@ -152,7 +154,7 @@ export default function DetailDoctor() {
           />
         )}
       </div>
-      <div>
+      <div className="pt-4 md:pt-0">
         <hr />
       </div>
       <div className=" md:pt-10 pt-20 pb-8 rounded-2xl">
